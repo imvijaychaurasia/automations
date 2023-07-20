452c34d48ee0f6b09b7e829409fc80cc5ede061e
Vijay Chaurasia <vijay@bizongo.com>
2023-07-20 10:18:52 +0530

    remove

--- swap-enable-jenkins.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-VMNAME=$(echo rails@${dev_vm}.indopus.in)
-echo $VMNAME
-ssh -o StrictHostKeyChecking=no $VMNAME "sudo fallocate -l 16G /swapfile"
-ssh -o StrictHostKeyChecking=no $VMNAME "sudo chmod 600 /swapfile"
-ssh -o StrictHostKeyChecking=no $VMNAME "sudo mkswap /swapfile"
-ssh -o StrictHostKeyChecking=no $VMNAME "sudo swapon /swapfile"
-ssh -o StrictHostKeyChecking=no $VMNAME "sudo free -h"
-ssh -o StrictHostKeyChecking=no $VMNAME "echo "SWAP Memory Has been deployed with size 16GB !!""
--- vm-env.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-NAME=$(echo rails@${qa_server}.indopus.in)
-echo $NAME
-
-echo "Configuring the Dev Environment"
-
-ssh -o StrictHostKeyChecking=no $NAME "sudo sed -i 's/dev-vm17/$qa_server/g' /etc/nginx/sites-enabled/* /etc/environment"
-ssh -o StrictHostKeyChecking=no $NAME "sed -i 's/dev-vm17/$qa_server/g' ./apps/micro-ticketing/.env ./apps/cataloguing-micro-frontend/.env ./apps/ums-micro-frontend/.env ./apps/go-partner/.env ./apps/reporting-micro-frontend/.env ./apps/Lead-Plus/.env ./apps/deliveries-micro-frontend/.env ./apps/configuration-ui/.env ~/apps/./vendor-management-frontend/.env ~/apps/./Partner-Hub/.env ~/apps/./transporter-app/.env ~/apps/./finance/.env ~/apps/artwork-frontend/.env ~/apps/./micro-procure-live/.env ~/apps/./procure-live/.env ~/apps/./ui-infra/.env ~/apps/./go-ops/.env ~/apps/./DCMS/.env"
