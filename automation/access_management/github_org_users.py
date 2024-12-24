0a7a3a0144fd21fa52aa9d07c5dbdfbb9e6e6e94
imvjai <130627325+imvjai@users.noreply.github.com>
2024-12-24 09:55:06 +0530

    Update and rename bizongo_org_github_users.py to github_org_users.py

--- automation/access_management/github_org_users.py
+++ automation/access_management/github_org_users.py
@@ -2,5 +2,5 @@ import requests
 from github import Github
 
 g = Github("org_admin_github_token")
-for member in g.get_organization("bizongo").get_members():
+for member in g.get_organization("orgname").get_members(): #replace org name
     print(member.login, member.name, member.email)
